2d344260db6c68e2beb559b6caf15b46ef10b5b6
toriimiyukki <toriimiyukki@gmail.com>
2012-01-05 18:27:19 +0900

    i have a misstake

--- rpstest.php
+++ rpstest.php
@@ -37,7 +37,7 @@ $t->is($rps->judge(), 1,'judgeの利用-payer:rock computer:scissor');
 
 $rps->setPlayerHand($rps->HANDS[1]);
 $rps->setComputerHand($rps->HANDS[0]);
-$t->is($rps->judge(), 1,'judgeの利用-payer:paper computer:rock');
+$t->is($rps->judge(), 0,'judgeの利用-payer:paper computer:rock');
 $rps->setPlayerHand($rps->HANDS[1]);
 $rps->setComputerHand($rps->HANDS[1]);
 $t->is($rps->judge(), 0,'judgeの利用-payer:paper computer:paper');
